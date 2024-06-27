94c1a14432a81d3dba85d5a48cc3acfb10d5d474
cal250 <jeancalvin027@gmail.com>
2024-06-27 05:37:01 +0200

    auth

--- controllers/jwtAuth.js
+++ controllers/jwtAuth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const userModal = require('../models/usermodel');
+// const userModal = require('../models/usermodel');
 
 const router = express.Router();
 
